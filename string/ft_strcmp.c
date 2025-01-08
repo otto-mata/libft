83e8bdb1288e7af49a29aedac06075034c0ba40d
otto-mata <ottomata@proton.me>
2025-01-08 23:15:18 +0100

    fix strcmp

--- string/ft_strcmp.c
+++ string/ft_strcmp.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tblochet <tblochet@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/09 11:01:51 by tblochet          #+#    #+#             */
-/*   Updated: 2025/01/01 19:27:08 by tblochet         ###   ########.fr       */
+/*                                                                            */
+/*   ft_strcmp.c                                          ┌─┐┌┬┐┌┬┐┌─┐        */
+/*                                                        │ │ │  │ │ │        */
+/*   By: tblochet <tblochet@student.42.fr>                └─┘ ┴  ┴ └─┘        */
+/*                                                        ┌┬┐┌─┐┌┬┐┌─┐        */
+/*   Created: 2024/11/09 11:01:51 by tblochet             │││├─┤ │ ├─┤        */
+/*   Updated: 2025/01/08 23:02:43 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_string.h"
 
-static size_t	ft_min(size_t a, size_t b)
+static size_t	ft_max(size_t a, size_t b)
 {
-	if (a < b)
+	if (a > b)
 		return (a);
 	return (b);
 }
 
 int	ft_strcmp(char const *s1, char const *s2)
 {
-	return (ft_memcmp(s1, s2, ft_min(ft_strlen(s1), ft_strlen(s2))));
+	return (ft_memcmp(s1, s2, ft_max(ft_strlen(s1), ft_strlen(s2))));
 }
