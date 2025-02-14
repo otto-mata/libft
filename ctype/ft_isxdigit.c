61b3c33a4cc4827dd81d793965a7f5276bd2534b
otto-mata <ottomata@proton.me>
2025-02-14 09:26:27 +0100

    Fix xdigit thing

--- ctype/ft_isxdigit.c
+++ ctype/ft_isxdigit.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_isxdigit.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tblochet <tblochet@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/09 11:01:51 by tblochet          #+#    #+#             */
-/*   Updated: 2025/01/01 20:39:40 by tblochet         ###   ########.fr       */
+/*                                                                            */
+/*   ft_isxdigit.c                                        ┌─┐┌┬┐┌┬┐┌─┐        */
+/*                                                        │ │ │  │ │ │        */
+/*   By: tblochet <tblochet@student.42.fr>                └─┘ ┴  ┴ └─┘        */
+/*                                                        ┌┬┐┌─┐┌┬┐┌─┐        */
+/*   Created: 2024/11/09 11:01:51 by tblochet             │││├─┤ │ ├─┤        */
+/*   Updated: 2025/02/14 09:26:00 by tblochet             ┴ ┴┴ ┴ ┴ ┴ ┴        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	ft_isxdigit(int c)
 	int	lower_c;
 
 	lower_c = ft_tolower(c);
-	return (ft_isdigit(c) || (c >= 'a' && c <= 'f'));
+	return (ft_isdigit(c) || (lower_c >= 'a' && lower_c <= 'f'));
 }
